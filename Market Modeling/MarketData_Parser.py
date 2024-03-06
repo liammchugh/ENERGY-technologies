a6006c333efcaab13f79c12a9ff6aaef054789ec
Liam McHugh <103697932+liammchugh@users.noreply.github.com>
2024-03-05 22:11:49 -0800

    market modeling setup, submodule to example LSTM added

--- Market Modeling/MarketData_Parser.py
+++ Market Modeling/MarketData_Parser.py
@@ -1,10 +1,24 @@
+#POWER GRID DATA PARSING
+
+#import packages
 import pandas as pd
 from datetime import datetime
-import pandas as pd
+
+#import useful ML packages
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+import numpy as np
+import keras.backend as kb
+import tensorflow as tf
+
+physical_devices = tf.config.list_physical_devices('GPU')
+if not physical_devices:
+    print("No GPU available.")
 
 # Given the structure, we'll now proceed to read the entire CSV and parse the data as previously done
 # but using the CSV reader for flexibility to handle varying data lengths
-file_path = r"C:\Users\liams\Documents\GitHub\ENERGY\Market Modeling\Data\20230306-20240305 CAISO Real-time Price.csv"
+CAISO_file_path = r"C:\Users\liams\Documents\GitHub\ENERGY\Market Modeling\Data\20230306-20240305 CAISO Real-time Price.csv"
 
 #PARSE MARKET DATA
 def parse_market_data(file_path):
@@ -22,16 +36,19 @@ def parse_market_data(file_path):
     df_full['day'] = df_full['Date'].dt.day
     df_full['time'] = df_full['Date'].dt.hour * 60 + df_full['Date'].dt.minute
 
-    # Convert month and day to day of year, considering leap years
+    # Convert month and day to day of year.
+    #INVESTIGATE EFFECT OF LEAP DAY, MINUTE, SECOND
     df_full['day_of_year'] = df_full['Date'].dt.dayofyear
 
     # Splitting data by hub
+    #Current Input Variables: Year, Month, day_of_year, Time,
+    #MORE IMPORTANT VARIABLES TO CONSIDER: LOCATION, DAY OF WEEK, WEATHER/TEMP CONDITIONS, GENERATOR OUTAGES, COMMODITY PRICES
     th_np15 = df_full[df_full['hub'] == 'TH_NP15'][['year', 'month', 'day', 'day_of_year', 'time', 'price']]
     th_sp15 = df_full[df_full['hub'] == 'TH_SP15'][['year', 'month', 'day', 'day_of_year', 'time', 'price']]
     th_zp26 = df_full[df_full['hub'] == 'TH_ZP26'][['year', 'month', 'day', 'day_of_year', 'time', 'price']]
 
     return th_np15, th_sp15, th_zp26
-th_np15, th_sp15, th_zp26 = parse_market_data(file_path)
+th_np15, th_sp15, th_zp26 = parse_market_data(CAISO_file_path)
 
 #DISPLAY SAMPLE
 def print_sample_data(samplesz, th_np15, th_sp15, th_zp26):
@@ -44,3 +61,7 @@ def print_sample_data(samplesz, th_np15, th_sp15, th_zp26):
     print(th_zp26_sample_updated)
 print_sample_data(3, th_np15, th_sp15, th_zp26)
 
+
+#TAKE DATA AND IMPLEMENT PREDICTIVE MODEL. LSTM RECURENT NN MODEL PROBABLY BEST. SEE DOCUMENTATION ON KERAS
+
+#EXAMPLE FROM 
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ENERGY
- Energy, Grid, Storage, Generation, Market Analysis
+ Energy, Grid, Storage, Generation, Market Analysis!
+
+
+## Environment Setup
+Follow these steps to set up your environment:
+1. Navigate to the github project directory
+2. Create a virtual environment (recommended for running analysis files):
+    python -m venv env
+3. Activate the virtual environment:
+    - On Windows:
+    ```
+    env\Scripts\activate
+    ```
+    - On Unix or MacOS:
+    ```
+    source env/bin/activate
+    ```
+4. Install the required packages:
+    pip install -r requirements.txt
+
+
